b4cdebcfe0146871be0917e6b62badf2fd44b803
Ziyan Jerry Chen <jerryc05@qq.com>
2019-10-12 23:39:08 -0400

    init push
    Signed-off-by: Ziyan "Jerry" Chen <jerryc05@qq.com>

---
+++ app/src/main/java/io/jerryc05/e2eeme/MainActivity.java
@@ -0,0 +1,14 @@
+package io.jerryc05.e2eeme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}

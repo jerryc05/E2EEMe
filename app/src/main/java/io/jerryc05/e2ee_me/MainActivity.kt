226b284bb12f8490d647f960cf868e312ac33e65
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-02-08 20:17:40 -0500

    switch to kotlin

---
+++ app/src/main/java/io/jerryc05/e2ee_me/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.jerryc05.e2ee_me
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
